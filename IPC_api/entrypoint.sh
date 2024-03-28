ae10e7890999f3fbca8157dbe2f545d4b43c722c
? <63070230@it.kmitl.ac.th>
2024-03-28 13:56:49 +0700

    docker 0.5

---
+++ IPC_api/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Collecting static files.."
+python manage.py collectstatic --noinput
+
+if [ "$RUN_MIGRATIONS" = "True" ]; then
+    echo "Running migrations..."
+    until python manage.py migrate
+    do
+        echo "Waiting for db to be ready..."
+        sleep 2
+    done
+fi
+
+echo "Starting app server..."
+python -m gunicorn railway_django_stack.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --log-level info \
+    --forwarded-allow-ips "*"
