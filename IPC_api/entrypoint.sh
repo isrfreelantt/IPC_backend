1722e34bb6aa92cbc84cd9921bdc28a03c9422cc
? <63070230@it.kmitl.ac.th>
2024-03-28 15:07:12 +0700

    Docker 0.9.1

--- IPC_api/entrypoint.sh
+++ IPC_api/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo 'Running collecstatic...'
-python manage.py collectstatic --no-input --settings=config.settings.production
+python manage.py collectstatic --no-input --settings=ipc_api.settings
 
 echo 'Applying migrations...'
-python manage.py wait_for_db --settings=config.settings.production
-python manage.py migrate --settings=config.settings.production
+python manage.py wait_for_db --settings=ipc_api.settings
+python manage.py migrate --settings=ipc_api.settings
 
 echo 'Running server...'
-gunicorn --env DJANGO_SETTINGS_MODULE=config.settings.production config.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn --env DJANGO_SETTINGS_MODULE=ipc_api.settings ipc_api.wsgi:application --bind 0.0.0.0:$PORT
